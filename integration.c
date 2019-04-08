1c6b6185c4e08e97c5681b8fc8361ec29676c1b5
slimisarra <47771183+slimisarra@users.noreply.github.com>
2019-04-08 08:51:10 -0700

    Add files via upload

--- integration.c
+++ integration.c
@@ -2,19 +2,66 @@
 #include<stdlib.h>
 #include<SDL/SDL.h>
 #include"ennemi.h"
+#include"objet.h"
+#include"background"
+#include"perso1.h"
+#include"perso2.h"
+#include"enigme.h"
 
 int main (void)
 {
 
 ennemi tab[15];
+meat m[10];
+bone b[10];
 int i;
 SDL_Init(SDL_INIT_VIDEO);
+
+
+initialiser_background();
+afficher_background();
+
+for(i=0;i<20;i++)
+{
+initialier_objet(&m[i]);
+afficher_objet(&m[i]);
+
+initialiser_objet(&b[i]);
+afficher_objet(&b[i]);
+}
+
+
 
 for(i=0;i<15;i++)
 {
 initialiser_afficher(&tab[i]);
 deplacer_ennemi(&tab[i]);
+
 }
+
+
+initialiser_afficher(Perso * entity);
+switch (case)
+{
+case 1 : deplacement_clavier(&entity , &background);
+         break;
+
+case 2 : deplacement_souris(&entity , &background);
+         break;
+}
+
+
+if collisionBox(objet,perso)
+{
+  afficher_enigme;
+  if (verifier_enigme==1)
+   {
+   x=score();
+   x=++;
+   }
+ }
+
+
 
 SDL_Quit() ;
 
