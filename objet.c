49ce3a68b9d2204f3400bf49695ce4f6c4dcd5da
CyrineLouati <47759860+CyrineLouati@users.noreply.github.com>
2019-05-05 14:59:25 +0200

    Update objet.c

--- objet.c
+++ objet.c
@@ -16,13 +16,13 @@ SDL_Surface* init_affich_objet( SDL_Surface *image ,SDL_Rect *Oposition,  int x 
          printf ( "Can not load image of tux: %s \n " , SDL_GetError ());
          exit ( 1 ); //erreur du chargement de l image
      }
-     else  { SDL_BlitSurface(image, NULL, ecran,&Oposition);
+     else  { SDL_BlitSurface(&image, NULL, ecran,&Oposition);
              SDL_Flip(ecran);
              Oposition->x =x; //initialisation de  objet a la position x,y
              Oposition->y =y;}
 return image ;
-   //SDL_FreeSurface(image);
-    SDL_Quit();}
+   //SDL_FreeSurface(&image);
+    //SDL_Quit();}
 
 //Collision trigo
 
@@ -47,9 +47,9 @@ else return false ;}
 
 
 int modifscore(SDL_Surface *Player,SDL_Rect Pposition,SDL_Surface scoreplus,SDL_Surface scoremoins,int vie,SDL_Rect sc_position)
-{int i;
+{int i;SDL_Surface *image;
 for (i=0;i++;i<20)
-{if (CollisionTrigoInsc(*Player,Meat[i].image1,Pposition,Meat[i].position1)||(CollisionTrigoCir(*Player,Meat[i].image1,Pposition,Meat[i].position1)))
+{if (CollisionTrigoInsc(&Player,Meat[i].image1,Pposition,Meat[i].position1)||(CollisionTrigoCir(&Player,Meat[i].image1,Pposition,Meat[i].position1)))
     if (vie>0 && vie<3) //Max de vie =3
 	    vie=vie+1; //Le joueur a collecté l objet qui lui additionne une vie
     image = IMG_Load("scoreplus.png");
@@ -57,8 +57,8 @@ for (i=0;i++;i<20)
      sc_position.y=(1300/2);
      SDL_BlitSurface(scoreplus,NULL,ecran,&sc_position);
      SDL_Flip(ecran);//Mise a jour de l ecran
-     SDL_FreeSurface (image);//disparition de l image du signal
-     SDL_FreeSurface (Meat[i].image1);
+     SDL_FreeSurface (&image);//disparition de l image du signal
+     SDL_FreeSurface (Meat[i].image1); //disparition de l objet
 
 if ((CollisionTrigoInsc(*Player,Bone[i].image2,Pposition,Bone[i].position2))||(CollisionTrigoCir(*Player,Bone[i].image2,Pposition,Bone[i].position2))
     vie=vie-1; //Le joueur a collecté l objet qui lui soustrait une vie
@@ -67,6 +67,6 @@ if ((CollisionTrigoInsc(*Player,Bone[i].image2,Pposition,Bone[i].position2))||(C
      sc_position.y=(1300/2);
      SDL_BlitSurface(scoremoins,NULL,ecran,&sc_position);
      SDL_Flip(ecran);
-     SDL_FreeSurface (image);//disparition de l image du signal
-     SDL_FreeSurface (Bone[i].image2);};
+     SDL_FreeSurface (&image);//disparition de l image du signal
+     SDL_FreeSurface (Bone[i].image2);}; //disparition de l objet
 return vie}
